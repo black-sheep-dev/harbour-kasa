9ed8a3f5507a26c3159b7a9f3f707829217bb634
Samuel Kron <blacksheep@nubecula.org>
2021-12-18 12:16:47 +0100

    Update translation

--- translations/harbour-kasa.ts
+++ translations/harbour-kasa.ts
@@ -1,347 +1,347 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="en_US">
 <context>
     <name></name>
     <message id="id-no-device-available">
         <source>No devices available</source>
-        <translation type="unfinished"></translation>
+        <translation>No devices available</translation>
     </message>
     <message id="id-offline">
         <source>Offline</source>
-        <translation type="unfinished"></translation>
+        <translation>Offline</translation>
     </message>
     <message id="id-add">
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Add</translation>
     </message>
     <message id="id-add-new-device">
         <source>Add new device</source>
-        <translation type="unfinished"></translation>
+        <translation>Add new device</translation>
     </message>
     <message id="id-enter-hostname">
         <source>Enter hostname</source>
-        <translation type="unfinished"></translation>
+        <translation>Enter hostname</translation>
     </message>
     <message id="id-hostname">
         <source>Hostname</source>
-        <translation type="unfinished"></translation>
+        <translation>Hostname</translation>
     </message>
     <message id="id-login">
         <source>Login</source>
-        <translation type="unfinished"></translation>
+        <translation>Login</translation>
     </message>
     <message id="id-cloud-login">
         <source>Cloud Login</source>
-        <translation type="unfinished"></translation>
+        <translation>Cloud Login</translation>
     </message>
     <message id="id-username">
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>Username</translation>
     </message>
     <message id="id-enter-username">
         <source>Enter username</source>
-        <translation type="unfinished"></translation>
+        <translation>Enter username</translation>
     </message>
     <message id="id-password">
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Password</translation>
     </message>
     <message id="id-enter-password">
         <source>Enter password</source>
-        <translation type="unfinished"></translation>
+        <translation>Enter password</translation>
     </message>
     <message id="id-about">
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>About</translation>
     </message>
     <message id="id-about-text">
         <source>Kasa is an app to control TP-Links Kasa smarthome devices.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kasa is an app to control TP-Links Kasa smarthome devices.</translation>
     </message>
     <message id="id-translations">
         <source>Translations</source>
-        <translation type="unfinished"></translation>
+        <translation>Translations</translation>
     </message>
     <message id="id-transltations-info">
         <source>Your language is not available? You are welcome to support this project by translating it on my self hosted Weblate server.</source>
-        <translation type="unfinished"></translation>
+        <translation>Your language is not available? You are welcome to support this project by translating it on my self hosted Weblate server.</translation>
     </message>
     <message id="id-sources">
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>Sources</translation>
     </message>
     <message id="id-donations">
         <source>Donations</source>
-        <translation type="unfinished"></translation>
+        <translation>Donations</translation>
     </message>
     <message id="id-buy-beer">
         <source>If you like my work why not buy me a beer?</source>
-        <translation type="unfinished"></translation>
+        <translation>If you like my work why not buy me a beer?</translation>
     </message>
     <message id="id-donate-paypal">
         <source>Donate with PayPal</source>
-        <translation type="unfinished"></translation>
+        <translation>Donate with PayPal</translation>
     </message>
     <message id="id-donate-liberpay">
         <source>Donate with Liberpay</source>
-        <translation type="unfinished"></translation>
+        <translation>Donate with Liberpay</translation>
     </message>
     <message id="id-factory-reset">
         <source>Factory Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Factory Reset</translation>
     </message>
     <message id="id-starting-factory-reset">
         <source>Starting factory reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Starting factory reset</translation>
     </message>
     <message id="id-reset-energy-data">
         <source>Reset energy statistic data</source>
-        <translation type="unfinished"></translation>
+        <translation>Reset energy statistic data</translation>
     </message>
     <message id="id-starting-statistic-reset">
         <source>Starting statistic reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Starting statistic reset</translation>
     </message>
     <message id="id-save">
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Save</translation>
     </message>
     <message id="id-expert-settings">
         <source>Expert Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Expert Settings</translation>
     </message>
     <message id="id-enable-expert-settings">
         <source>Enable expert settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Enable expert settings</translation>
     </message>
     <message id="id-enable-expert-settings-info">
         <source>Only enable expert settings when you know what you are doing! You can brick your device! I have warned you!</source>
-        <translation type="unfinished"></translation>
+        <translation>Only enable expert settings when you know what you are doing! You can brick your device! I have warned you!</translation>
     </message>
     <message id="id-hardware">
         <source>Hardware</source>
-        <translation type="unfinished"></translation>
+        <translation>Hardware</translation>
     </message>
     <message id="id-mac-address">
         <source>MAC address</source>
-        <translation type="unfinished"></translation>
+        <translation>MAC address</translation>
     </message>
     <message id="id-enter-mac-address">
         <source>Enter MAC address</source>
-        <translation type="unfinished"></translation>
+        <translation>Enter MAC address</translation>
     </message>
     <message id="id-cloud">
         <source>Cloud</source>
-        <translation type="unfinished"></translation>
+        <translation>Cloud</translation>
     </message>
     <message id="id-cloud-server-url">
         <source>Cloud Server URL</source>
-        <translation type="unfinished"></translation>
+        <translation>Cloud Server URL</translation>
     </message>
     <message id="id-enter-cloud-server-url">
         <source>Enter Cloud Server URL</source>
-        <translation type="unfinished"></translation>
+        <translation>Enter Cloud Server URL</translation>
     </message>
     <message id="id-cloud-url-info">
         <source>Changing the cloud server url will prevent the device from sending data home (not yours). Hello privacy!</source>
-        <translation type="unfinished"></translation>
+        <translation>Changing the cloud server url will prevent the device from sending data home (not yours). Hello privacy!</translation>
     </message>
     <message id="id-cloud-default-url-info">
         <source>The default url is &apos;devs.tplinkcloud.com&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>The default url is &apos;devs.tplinkcloud.com&apos;</translation>
     </message>
     <message id="id-settings">
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Settings</translation>
     </message>
     <message id="id-enery-consumtion">
         <source>Energy Consumption</source>
-        <translation type="unfinished"></translation>
+        <translation>Energy Consumption</translation>
     </message>
     <message id="id-refresh">
         <source>Refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>Refresh</translation>
     </message>
     <message id="id-device-info">
         <source>Device Info</source>
-        <translation type="unfinished"></translation>
+        <translation>Device Info</translation>
     </message>
     <message id="id-turn-on-off">
         <source>Turn on/off</source>
-        <translation type="unfinished"></translation>
+        <translation>Turn on/off</translation>
     </message>
     <message id="id-brightness">
         <source>Brightness</source>
-        <translation type="unfinished"></translation>
+        <translation>Brightness</translation>
     </message>
     <message id="id-general">
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>General</translation>
     </message>
     <message id="id-name">
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Name</translation>
     </message>
     <message id="id-description">
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>Description</translation>
     </message>
     <message id="id-model">
         <source>Model</source>
-        <translation type="unfinished"></translation>
+        <translation>Model</translation>
     </message>
     <message id="id-device-id">
         <source>Device ID</source>
-        <translation type="unfinished"></translation>
+        <translation>Device ID</translation>
     </message>
     <message id="id-hw-version">
         <source>HW Version</source>
-        <translation type="unfinished"></translation>
+        <translation>HW Version</translation>
     </message>
     <message id="id-firmware">
         <source>Firmware</source>
-        <translation type="unfinished"></translation>
+        <translation>Firmware</translation>
     </message>
     <message id="id-network">
         <source>Network</source>
-        <translation type="unfinished"></translation>
+        <translation>Network</translation>
     </message>
     <message id="id-mac">
         <source>MAC</source>
-        <translation type="unfinished"></translation>
+        <translation>MAC</translation>
     </message>
     <message id="id-rssi">
         <source>RSSI</source>
-        <translation type="unfinished"></translation>
+        <translation>RSSI</translation>
     </message>
     <message id="id-energy">
         <source>Energy</source>
         <extracomment>Electric energy</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Energy</translation>
     </message>
     <message id="id-current">
         <source>Current</source>
         <extracomment>Electric current</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Current</translation>
     </message>
     <message id="id-power">
         <source>Power</source>
         <extracomment>Electric power</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Power</translation>
     </message>
     <message id="id-voltage">
         <source>Voltage</source>
         <extracomment>Electric voltage</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Voltage</translation>
     </message>
     <message id="id-total">
         <source>Total</source>
-        <translation type="unfinished"></translation>
+        <translation>Total</translation>
     </message>
     <message id="id-add-device">
         <source>Add Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Add Device</translation>
     </message>
     <message id="id-device-offline-or-error">
         <source>Device offline or network error</source>
-        <translation type="unfinished"></translation>
+        <translation>Device offline or network error</translation>
     </message>
     <message id="id-delete">
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Delete</translation>
     </message>
     <message id="id-send-commands">
         <source>Send commands</source>
-        <translation type="unfinished"></translation>
+        <translation>Send commands</translation>
     </message>
     <message id="id-no-devices-available">
         <source>No devices available</source>
-        <translation type="unfinished"></translation>
+        <translation>No devices available</translation>
     </message>
     <message id="id-no-devices-hint">
         <source>Pull down to add a new device</source>
-        <translation type="unfinished"></translation>
+        <translation>Pull down to add a new device</translation>
     </message>
     <message id="id-restart-device">
         <source>Restart Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Restart Device</translation>
     </message>
     <message id="id-device-settings">
         <source>Device Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Device Settings</translation>
     </message>
     <message id="id-device-alias">
         <source>Device Alias</source>
-        <translation type="unfinished"></translation>
+        <translation>Device Alias</translation>
     </message>
     <message id="id-enter-device-alias">
         <source>Enter device alias</source>
-        <translation type="unfinished"></translation>
+        <translation>Enter device alias</translation>
     </message>
     <message id="Hardware">
         <source>Hardware</source>
-        <translation type="unfinished"></translation>
+        <translation>Hardware</translation>
     </message>
     <message id="id-toggle-led">
         <source>Turn LED on/off</source>
-        <translation type="unfinished"></translation>
+        <translation>Turn LED on/off</translation>
     </message>
     <message id="id-device-cloud-registered">
         <source>Device registered in cloud</source>
-        <translation type="unfinished"></translation>
+        <translation>Device registered in cloud</translation>
     </message>
     <message id="id-device-cloud-not-registered">
         <source>Device is not registered in cloud</source>
-        <translation type="unfinished"></translation>
+        <translation>Device is not registered in cloud</translation>
     </message>
     <message id="id-register-info-text">
         <source>You can register or unregister the device from TP-Link cloud. For registration you need to provide username and password.</source>
-        <translation type="unfinished"></translation>
+        <translation>You can register or unregister the device from TP-Link cloud. For registration you need to provide username and password.</translation>
     </message>
     <message id="id-leaving-cloud">
         <source>Leaving cloud</source>
-        <translation type="unfinished"></translation>
+        <translation>Leaving cloud</translation>
     </message>
     <message id="id-month-view">
         <source>Month View</source>
-        <translation type="unfinished"></translation>
+        <translation>Month View</translation>
     </message>
     <message id="id-year-view">
         <source>Year View</source>
-        <translation type="unfinished"></translation>
+        <translation>Year View</translation>
     </message>
     <message id="id-energy-consumption">
         <source>Energy Consumption</source>
-        <translation type="unfinished"></translation>
+        <translation>Energy Consumption</translation>
     </message>
     <message id="id-send-command">
         <source>Send command</source>
-        <translation type="unfinished"></translation>
+        <translation>Send command</translation>
     </message>
     <message id="id-send">
         <source>Send</source>
-        <translation type="unfinished"></translation>
+        <translation>Send</translation>
     </message>
     <message id="id-clear">
         <source>Clear</source>
-        <translation type="unfinished"></translation>
+        <translation>Clear</translation>
     </message>
     <message id="id-debug-api-traffic">
         <source>Debug API Traffic</source>
-        <translation type="unfinished"></translation>
+        <translation>Debug API Traffic</translation>
     </message>
     <message id="id-debug-info-1">
         <source>This option will log all traffic between devices and this app into Documents folder.</source>
-        <translation type="unfinished"></translation>
+        <translation>This option will log all traffic between devices and this app into Documents folder.</translation>
     </message>
     <message id="id-debug-info-2">
         <source>The data will help to investigate errors or to improve this app.</source>
-        <translation type="unfinished"></translation>
+        <translation>The data will help to investigate errors or to improve this app.</translation>
     </message>
     <message id="id-devices">
         <source>Devices</source>
-        <translation type="unfinished"></translation>
+        <translation>Devices</translation>
     </message>
 </context>
 </TS>
