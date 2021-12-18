9c5c086fd1b159813073e9a79d1c85e8c4fecb0d
Weblate Admin <blacksheep@nubecula.org>
2021-12-18 11:55:12 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (84 of 84 strings)
    
    Translation: harbour-kasa/harbour-kasa
    Translate-URL: http://weblate.nubecula.org/projects/harbour-kasa/harbour-kasa/de/

--- translations/harbour-kasa-de.ts
+++ translations/harbour-kasa-de.ts
@@ -5,11 +5,11 @@
     <name></name>
     <message id="id-no-device-available">
         <source>No devices available</source>
-        <translation type="unfinished">Keine Geräte vorhanden</translation>
+        <translation>Keine Geräte vorhanden</translation>
     </message>
     <message id="id-offline">
         <source>Offline</source>
-        <translation type="unfinished">Offline</translation>
+        <translation>Offline</translation>
     </message>
     <message id="id-add">
         <source>Add</source>
@@ -17,7 +17,7 @@
     </message>
     <message id="id-add-new-device">
         <source>Add new device</source>
-        <translation type="unfinished">Neues Gerät hinzufügen</translation>
+        <translation>Neues Gerät hinzufügen</translation>
     </message>
     <message id="id-enter-hostname">
         <source>Enter hostname</source>
@@ -29,11 +29,11 @@
     </message>
     <message id="id-login">
         <source>Login</source>
-        <translation type="unfinished">Anmelden</translation>
+        <translation>Anmelden</translation>
     </message>
     <message id="id-cloud-login">
         <source>Cloud Login</source>
-        <translation type="unfinished">Cloud Anmeldung</translation>
+        <translation>Cloud Anmeldung</translation>
     </message>
     <message id="id-username">
         <source>Username</source>
@@ -57,11 +57,11 @@
     </message>
     <message id="id-about-text">
         <source>Kasa is an app to control TP-Links Kasa smarthome devices.</source>
-        <translation type="unfinished">Kasa ist eine App um TP-Link Kasa Smart Home Geräte zu steuern.</translation>
+        <translation>Kasa ist eine App um TP-Link Kasa Smart Home Geräte zu steuern.</translation>
     </message>
     <message id="id-translations">
         <source>Translations</source>
-        <translation type="unfinished">Übersetzungen</translation>
+        <translation>Übersetzungen</translation>
     </message>
     <message id="id-transltations-info">
         <source>Your language is not available? You are welcome to support this project by translating it on my self hosted Weblate server.</source>
@@ -69,11 +69,11 @@
     </message>
     <message id="id-sources">
         <source>Sources</source>
-        <translation type="unfinished">Quellen</translation>
+        <translation>Quellen</translation>
     </message>
     <message id="id-donations">
         <source>Donations</source>
-        <translation type="unfinished">Spenden</translation>
+        <translation>Spenden</translation>
     </message>
     <message id="id-buy-beer">
         <source>If you like my work why not buy me a beer?</source>
@@ -81,267 +81,267 @@
     </message>
     <message id="id-donate-paypal">
         <source>Donate with PayPal</source>
-        <translation type="unfinished">Spenden via PayPal</translation>
+        <translation>Spenden via PayPal</translation>
     </message>
     <message id="id-donate-liberpay">
         <source>Donate with Liberpay</source>
-        <translation type="unfinished">Spenden via Liberpay</translation>
+        <translation>Spenden via Liberpay</translation>
     </message>
     <message id="id-factory-reset">
         <source>Factory Reset</source>
-        <translation type="unfinished">Werkseinstellungen zurücksetzen</translation>
+        <translation>Werkseinstellungen zurücksetzen</translation>
     </message>
     <message id="id-starting-factory-reset">
         <source>Starting factory reset</source>
-        <translation type="unfinished">Starte Zurücksetzen auf Werkseinstellungen</translation>
+        <translation>Starte Zurücksetzen auf Werkseinstellungen</translation>
     </message>
     <message id="id-reset-energy-data">
         <source>Reset energy statistic data</source>
-        <translation type="unfinished">Energie Statistik zurücksetzen</translation>
+        <translation>Energie Statistik zurücksetzen</translation>
     </message>
     <message id="id-starting-statistic-reset">
         <source>Starting statistic reset</source>
-        <translation type="unfinished">Starte Zruücksetzen der Statistik</translation>
+        <translation>Starte Zruücksetzen der Statistik</translation>
     </message>
     <message id="id-save">
         <source>Save</source>
-        <translation type="unfinished">Speichern</translation>
+        <translation>Speichern</translation>
     </message>
     <message id="id-expert-settings">
         <source>Expert Settings</source>
-        <translation type="unfinished">Experten Einstellungen</translation>
+        <translation>Experten Einstellungen</translation>
     </message>
     <message id="id-enable-expert-settings">
         <source>Enable expert settings</source>
-        <translation type="unfinished">Experten Einstellungen aktivieren</translation>
+        <translation>Experten Einstellungen aktivieren</translation>
     </message>
     <message id="id-enable-expert-settings-info">
         <source>Only enable expert settings when you know what you are doing! You can brick your device! I have warned you!</source>
-        <translation type="unfinished">Aktiviere die Experteneinstellungen nur, wenn Du weißt was du tust! Du kannst dein Gerät unbrauchbar machen! Ich habe Dich gewarnt!</translation>
+        <translation>Aktiviere die Experteneinstellungen nur, wenn Du weißt was du tust! Du kannst dein Gerät unbrauchbar machen! Ich habe Dich gewarnt!</translation>
     </message>
     <message id="id-hardware">
         <source>Hardware</source>
-        <translation type="unfinished">Hardware</translation>
+        <translation>Hardware</translation>
     </message>
     <message id="id-mac-address">
         <source>MAC address</source>
-        <translation type="unfinished">MAC Adresse</translation>
+        <translation>MAC Adresse</translation>
     </message>
     <message id="id-enter-mac-address">
         <source>Enter MAC address</source>
-        <translation type="unfinished">MAC Adresse eingeben</translation>
+        <translation>MAC Adresse eingeben</translation>
     </message>
     <message id="id-cloud">
         <source>Cloud</source>
-        <translation type="unfinished">Cloud</translation>
+        <translation>Cloud</translation>
     </message>
     <message id="id-cloud-server-url">
         <source>Cloud Server URL</source>
-        <translation type="unfinished">Cloud Server URL</translation>
+        <translation>Cloud Server URL</translation>
     </message>
     <message id="id-enter-cloud-server-url">
         <source>Enter Cloud Server URL</source>
-        <translation type="unfinished">Cloud Server URL eingeben</translation>
+        <translation>Cloud Server URL eingeben</translation>
     </message>
     <message id="id-cloud-url-info">
         <source>Changing the cloud server url will prevent the device from sending data home (not yours). Hello privacy!</source>
-        <translation type="unfinished">Das Ändern der Cloud Server URL hindert das Gerät daran nach Hause zu telefonieren (nicht Deins). Willkommen Privatsphäre!</translation>
+        <translation>Das Ändern der Cloud Server URL hindert das Gerät daran nach Hause zu telefonieren (nicht Deines). Willkommen Privatsphäre!</translation>
     </message>
     <message id="id-cloud-default-url-info">
         <source>The default url is &apos;devs.tplinkcloud.com&apos;</source>
-        <translation type="unfinished">Die standard URL ist &apos;devs.tplinkcloud.com&apos;</translation>
+        <translation>Die standard URL ist &apos;devs.tplinkcloud.com&apos;</translation>
     </message>
     <message id="id-settings">
         <source>Settings</source>
-        <translation type="unfinished">Einstellungen</translation>
+        <translation>Einstellungen</translation>
     </message>
     <message id="id-enery-consumtion">
         <source>Energy Consumption</source>
-        <translation type="unfinished">Energieverbrauch</translation>
+        <translation>Energieverbrauch</translation>
     </message>
     <message id="id-refresh">
         <source>Refresh</source>
-        <translation type="unfinished">Aktualisieren</translation>
+        <translation>Aktualisieren</translation>
     </message>
     <message id="id-device-info">
         <source>Device Info</source>
-        <translation type="unfinished">Geräte Info</translation>
+        <translation>Geräte Info</translation>
     </message>
     <message id="id-turn-on-off">
         <source>Turn on/off</source>
-        <translation type="unfinished">Schalte an/aus</translation>
+        <translation>Schalte an/aus</translation>
     </message>
     <message id="id-brightness">
         <source>Brightness</source>
-        <translation type="unfinished">Helligkeit</translation>
+        <translation>Helligkeit</translation>
     </message>
     <message id="id-general">
         <source>General</source>
-        <translation type="unfinished">Allgemein</translation>
+        <translation>Allgemein</translation>
     </message>
     <message id="id-name">
         <source>Name</source>
-        <translation type="unfinished">Name</translation>
+        <translation>Name</translation>
     </message>
     <message id="id-description">
         <source>Description</source>
-        <translation type="unfinished">Beschreibung</translation>
+        <translation>Beschreibung</translation>
     </message>
     <message id="id-model">
         <source>Model</source>
-        <translation type="unfinished">Modell</translation>
+        <translation>Modell</translation>
     </message>
     <message id="id-device-id">
         <source>Device ID</source>
-        <translation type="unfinished">Geräte ID</translation>
+        <translation>Geräte ID</translation>
     </message>
     <message id="id-hw-version">
         <source>HW Version</source>
-        <translation type="unfinished">HW Version</translation>
+        <translation>HW Version</translation>
     </message>
     <message id="id-firmware">
         <source>Firmware</source>
-        <translation type="unfinished">Firmware</translation>
+        <translation>Firmware</translation>
     </message>
     <message id="id-network">
         <source>Network</source>
-        <translation type="unfinished">Netzwerk</translation>
+        <translation>Netzwerk</translation>
     </message>
     <message id="id-mac">
         <source>MAC</source>
-        <translation type="unfinished">MAC</translation>
+        <translation>MAC</translation>
     </message>
     <message id="id-rssi">
         <source>RSSI</source>
-        <translation type="unfinished">RSSI</translation>
+        <translation>RSSI</translation>
     </message>
     <message id="id-energy">
         <source>Energy</source>
         <extracomment>Electric energy</extracomment>
-        <translation type="unfinished">Energie</translation>
+        <translation>Energie</translation>
     </message>
     <message id="id-current">
         <source>Current</source>
         <extracomment>Electric current</extracomment>
-        <translation type="unfinished">Strom</translation>
+        <translation>Strom</translation>
     </message>
     <message id="id-power">
         <source>Power</source>
         <extracomment>Electric power</extracomment>
-        <translation type="unfinished">Leistung</translation>
+        <translation>Leistung</translation>
     </message>
     <message id="id-voltage">
         <source>Voltage</source>
         <extracomment>Electric voltage</extracomment>
-        <translation type="unfinished">Spannung</translation>
+        <translation>Spannung</translation>
     </message>
     <message id="id-total">
         <source>Total</source>
-        <translation type="unfinished">Total</translation>
+        <translation>Total</translation>
     </message>
     <message id="id-add-device">
         <source>Add Device</source>
-        <translation type="unfinished">Gerät hinzufügen</translation>
+        <translation>Gerät hinzufügen</translation>
     </message>
     <message id="id-device-offline-or-error">
         <source>Device offline or network error</source>
-        <translation type="unfinished">Gerät offline oder Netzwerkfehler</translation>
+        <translation>Gerät offline oder Netzwerkfehler</translation>
     </message>
     <message id="id-delete">
         <source>Delete</source>
-        <translation type="unfinished">Löschen</translation>
+        <translation>Löschen</translation>
     </message>
     <message id="id-send-commands">
         <source>Send commands</source>
-        <translation type="unfinished">Sende Kommandos</translation>
+        <translation>Sende Kommandos</translation>
     </message>
     <message id="id-no-devices-available">
         <source>No devices available</source>
-        <translation type="unfinished">Keine Geräte vorhanden</translation>
+        <translation>Keine Geräte vorhanden</translation>
     </message>
     <message id="id-no-devices-hint">
         <source>Pull down to add a new device</source>
-        <translation type="unfinished">Nach unten ziehen um ein neues Gerät hinzuzufügen</translation>
+        <translation>Nach unten ziehen um ein neues Gerät hinzuzufügen</translation>
     </message>
     <message id="id-restart-device">
         <source>Restart Device</source>
-        <translation type="unfinished">Gerät neustarten</translation>
+        <translation>Gerät neustarten</translation>
     </message>
     <message id="id-device-settings">
         <source>Device Settings</source>
-        <translation type="unfinished">Geräte Einstellungen</translation>
+        <translation>Geräte Einstellungen</translation>
     </message>
     <message id="id-device-alias">
         <source>Device Alias</source>
-        <translation type="unfinished">Gerätename</translation>
+        <translation>Gerätename</translation>
     </message>
     <message id="id-enter-device-alias">
         <source>Enter device alias</source>
-        <translation type="unfinished">Gerätenamen eingeben</translation>
+        <translation>Gerätenamen eingeben</translation>
     </message>
     <message id="Hardware">
         <source>Hardware</source>
-        <translation type="unfinished">Hardware</translation>
+        <translation>Hardware</translation>
     </message>
     <message id="id-toggle-led">
         <source>Turn LED on/off</source>
-        <translation type="unfinished">Schalte LED an/aus</translation>
+        <translation>Schalte LED an/aus</translation>
     </message>
     <message id="id-device-cloud-registered">
         <source>Device registered in cloud</source>
-        <translation type="unfinished">Gerät ist in der Cloud registriert</translation>
+        <translation>Gerät ist in der Cloud registriert</translation>
     </message>
     <message id="id-device-cloud-not-registered">
         <source>Device is not registered in cloud</source>
-        <translation type="unfinished">Gerät ist nicht in der Cloud registriert</translation>
+        <translation>Gerät ist nicht in der Cloud registriert</translation>
     </message>
     <message id="id-register-info-text">
         <source>You can register or unregister the device from TP-Link cloud. For registration you need to provide username and password.</source>
-        <translation type="unfinished">Du kannst das Geräte in der TP-Link Cloud registrieren oder abmelden. Für die Registrierung werden Benutzername und Password benötigt.</translation>
+        <translation>Du kannst das Geräte in der TP-Link Cloud registrieren oder abmelden. Für die Registrierung werden Benutzername und Password benötigt.</translation>
     </message>
     <message id="id-leaving-cloud">
         <source>Leaving cloud</source>
-        <translation type="unfinished">Verlasse Cloud</translation>
+        <translation>Verlasse Cloud</translation>
     </message>
     <message id="id-month-view">
         <source>Month View</source>
-        <translation type="unfinished">Monatsansicht</translation>
+        <translation>Monatsansicht</translation>
     </message>
     <message id="id-year-view">
         <source>Year View</source>
-        <translation type="unfinished">Jahresansicht</translation>
+        <translation>Jahresansicht</translation>
     </message>
     <message id="id-energy-consumption">
         <source>Energy Consumption</source>
-        <translation type="unfinished">Energieverbrauch</translation>
+        <translation>Energieverbrauch</translation>
     </message>
     <message id="id-send-command">
         <source>Send command</source>
-        <translation type="unfinished">Sende Kommando</translation>
+        <translation>Sende Kommando</translation>
     </message>
     <message id="id-send">
         <source>Send</source>
-        <translation type="unfinished">Senden</translation>
+        <translation>Senden</translation>
     </message>
     <message id="id-clear">
         <source>Clear</source>
-        <translation type="unfinished">Leeren</translation>
+        <translation>Leeren</translation>
     </message>
     <message id="id-debug-api-traffic">
         <source>Debug API Traffic</source>
-        <translation type="unfinished">Debug API Verkehr</translation>
+        <translation>Debug API Verkehr</translation>
     </message>
     <message id="id-debug-info-1">
         <source>This option will log all traffic between devices and this app into Documents folder.</source>
-        <translation type="unfinished">Diese Option loggt den gesamten Verkehr zwischen dem Gerät und dieser Anwendung in den Dokumenten Ordner.</translation>
+        <translation>Diese Option loggt den gesamten Verkehr zwischen dem Gerät und dieser Anwendung in den Dokumenten Ordner.</translation>
     </message>
     <message id="id-debug-info-2">
         <source>The data will help to investigate errors or to improve this app.</source>
-        <translation type="unfinished">Die Daten sollen dabei helfen, Fehler zu beheben oder die Anwendung zu verbessern.</translation>
+        <translation>Die Daten sollen dabei helfen, Fehler zu beheben oder die Anwendung zu verbessern.</translation>
     </message>
     <message id="id-devices">
         <source>Devices</source>
-        <translation type="unfinished">Gerät</translation>
+        <translation>Gerät</translation>
     </message>
 </context>
 <context>
